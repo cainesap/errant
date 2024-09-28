3aa6f8b81cad0f3f8c13e9671c9834682582ee3f
Christopher <christopher@writer.com>
2024-09-28 10:38:58 +0000

    Add rough German support

--- errant/__init__.py
+++ errant/__init__.py
@@ -8,7 +8,7 @@ __version__ = '3.0.0'
 # Load an ERRANT Annotator object for a given language
 def load(lang, nlp=None):
     # Make sure the language is supported
-    supported = {"en"}
+    supported = {"de", "en"}
     if lang not in supported:
         raise Exception(f"{lang} is an unsupported or unknown language")
 
@@ -16,12 +16,14 @@ def load(lang, nlp=None):
     nlp = nlp or spacy.load(f"{lang}_core_web_sm", disable=["ner"])
 
     # Load language edit merger
-    merger = import_module(f"errant.{lang}.merger")
+    if lang != "en":
+        print(f"WARNING: Loading English merger and classifier for language {lang}")
+    merger = import_module(f"errant.en.merger")
 
     # Load language edit classifier
-    classifier = import_module(f"errant.{lang}.classifier")
+    classifier = import_module(f"errant.en.classifier")
     # The English classifier needs spacy
-    if lang == "en": classifier.nlp = nlp
+    classifier.nlp = nlp
 
     # Return a configured ERRANT annotator
     return Annotator(lang, nlp, merger, classifier)
