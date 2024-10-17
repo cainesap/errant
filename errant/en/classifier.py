ab7eae458aa7929ea2ab23777b03f85cd4114aea
Andrew Caines <cainesap@users.noreply.github.com>
2024-10-17 16:07:28 +0100

    Update classifier.py

--- errant/en/classifier.py
+++ errant/en/classifier.py
@@ -6,7 +6,8 @@ import spacy.symbols as POS
 
 # Load Hunspell word list
 def load_word_list(path):
-    with open(path) as word_list:
+    #with open(path) as word_list:
+    with open(path, encoding='utf-8') as word_list:
         return set([word.strip() for word in word_list])
 
 # Load Universal Dependency POS Tags map file.
