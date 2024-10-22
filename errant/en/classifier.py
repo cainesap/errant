5af333e413cced172b7ab7313c20ee34aa40d13f
cainesap <cainesap@gmail.com>
2024-10-22 20:33:03 +0100

    errant

--- errant/en/classifier.py
+++ errant/en/classifier.py
@@ -118,12 +118,12 @@ def get_edit_info(toks):
     pos = []
     dep = []
     for tok in toks:
-        if len(tok.tag_) > 0:  # AC 2024-10-21: new if statement to account for empty pos-tags with UDPipe
+        if len(tok.tag_)>0:  # AC 2024-10-21: new if statement to account for empty pos-tags with UDPipe
             pos.append(pos_map[tok.tag_])
             dep.append(tok.dep_)
         else:
-            pos.append(pos_map[tok.tag_])
-            dep.append(tok.dep_)
+            pos.append("X")
+            dep.append("dep")
     return pos, dep
 
 # Input: Spacy tokens
