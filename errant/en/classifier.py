908d2e54577f09c0f48cd01a8bd6a69eb842cbee
Andrew Caines <cainesap@users.noreply.github.com>
2024-10-22 17:54:58 +0100

    Update classifier.py

--- errant/en/classifier.py
+++ errant/en/classifier.py
@@ -118,8 +118,12 @@ def get_edit_info(toks):
     pos = []
     dep = []
     for tok in toks:
-        pos.append(pos_map[tok.tag_])
-        dep.append(tok.dep_)
+        if len(tok.tag_) > 0:  # AC 2024-10-21: new if statement to account for empty pos-tags with UDPipe
+            pos.append(pos_map[tok.tag_])
+            dep.append(tok.dep_)
+        else:
+            pos.append(pos_map[tok.tag_])
+            dep.append(tok.dep_)
     return pos, dep
 
 # Input: Spacy tokens
