7611d4385a1933b565dc93bffc6cb4c14de17190
cainesap <cainesap@gmail.com>
2024-10-28 17:16:38 +0000

    errant

--- errant/__init__.py
+++ errant/__init__.py
@@ -26,7 +26,8 @@ def load(lang, nlp=None):
         #print(f"Ok, using the spaCy model {spacy_small} for {lang}")
         #spacy.load(spacy_small, disable=["ner"])
         print(f"Ok, using the UDPipe model in {lang} for spacy-udpipe")
-        nlp = spacy_udpipe.load(lang)
+        #nlp = spacy_udpipe.load(lang)
+        nlp = spacy_udpipe.load_from_path(lang, path="./")
     
     # Load language edit merger and edit classifier (use the English one for all)
     #if lang == "en":
